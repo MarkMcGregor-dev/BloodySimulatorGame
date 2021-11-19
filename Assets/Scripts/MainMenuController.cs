4f62ecb352ca6274d157eb300bc905fa107962c8
noreenqureshi <noreen.qureshi99@gmail.com>
2021-11-18 19:58:03 -0500

    Added HowToPlay scene
    *note for later: added a back button (to return to main menu) but for some reason it's not linking to the right scene

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -6,20 +6,53 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    public string InGameSceneName;
+    public string PlayScene;
+    public string InstructionsScene;
+    public string OptionsScene;
+    public string BackScene;
 
     private Button startButton;
+    private Button howToPlayButton;
+    private Button optionsButton;
+    private Button backButton;
 
     void Start()
     {
         // setup variables
         startButton = GameObject.Find("Start Button").GetComponent<Button>();
         startButton.onClick.AddListener(StartPressed);
+
+        howToPlayButton = GameObject.Find("HowTo Button").GetComponent<Button>();
+        howToPlayButton.onClick.AddListener(HowToPressed);
+
+        optionsButton = GameObject.Find("Options Button").GetComponent<Button>();
+        optionsButton.onClick.AddListener(OptionsPressed);
+
+        backButton = GameObject.Find("Back Button").GetComponent<Button>();
+        backButton.onClick.AddListener(BackPressed);
+
     }
 
     private void StartPressed()
     {
         // go to the in-game scene when pressed
-        SceneManager.LoadScene(InGameSceneName);
+        SceneManager.LoadScene(PlayScene);
+    }
+
+    private void HowToPressed()
+    {
+        // go to the in-game scene when pressed
+        SceneManager.LoadScene(InstructionsScene);
+    }
+
+    private void OptionsPressed()
+    {
+        // go to the in-game scene when pressed
+        SceneManager.LoadScene(OptionsScene);
+    }
+    private void BackPressed()
+    {
+        // go to the in-game scene when pressed
+        SceneManager.LoadScene(BackScene);
     }
 }
