5edf384f2d9565dd86e936bb75e5a0558f3a6fe9
Dylarno <medeirosdylan@hotmail.com>
2021-11-29 00:25:42 -0500

    Green glow when blockages are breakable

--- Assets/Prefabs/Blockage/BlockageBehaviour.cs
+++ Assets/Prefabs/Blockage/BlockageBehaviour.cs
@@ -7,6 +7,46 @@ public class BlockageBehaviour : MonoBehaviour
     public delegate void BlockageBreakDelegate();
     public static event BlockageBreakDelegate BlockageBroken;
 
+    private GameObject player;
+
+    private Material blockageMat;
+    private Color c;
+    private float a = 0.0f;
+    private bool goingUp;
+
+    private Color transparent = new Color(27, 229, 108, 0);
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+
+        blockageMat = transform.GetChild(0).GetComponent<MeshRenderer>().material;
+
+        c = blockageMat.GetColor("_BaseColor");
+        goingUp = true;
+    }
+
+    private void Update()
+    {
+        if (player.GetComponent<PlayerController>().numCellsCollected >= 5)
+        {
+            c.a = goingUp ? .005f + c.a : c.a - .005f;
+
+            if (c.a >= 0.5f)
+                goingUp = false;
+            else if (c.a <= 0.0f)
+                goingUp = true;
+
+           blockageMat.SetColor("_BaseColor", c);
+        }
+
+        else
+        {
+            blockageMat.SetColor("_BaseColor", transparent);
+        }
+        
+    }
+
     public void BreakBlockage()
     {
         // fire event
@@ -15,8 +55,7 @@ public class BlockageBehaviour : MonoBehaviour
         // destroy the object
         Destroy(gameObject);
 
-        GameObject Player = GameObject.Find("Player");
-        GameObject CollectedBloodCells = Player.transform.GetChild(0).gameObject.transform.GetChild(1).gameObject;
+        GameObject CollectedBloodCells = player.transform.GetChild(0).gameObject.transform.GetChild(1).gameObject;
 
         for (int i = 0; i < 5; i++)
         {
